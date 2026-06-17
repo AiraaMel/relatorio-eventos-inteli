b57ef503061e0c38a7ec9cf599af8fa69f005f44
Aira Mel <aira.souza@sou.inteli.edu.br>
2026-06-17 10:36:12 -0300

    feat4

--- config.js
+++
@@ -1,4 +0,0 @@
-const CONFIG = {
-  SHEETS_URL: "https://docs.google.com/spreadsheets/d/e/2PACX-1vQSQL_2kTy-wQqTOGbTDOpt3xoHkh_9ArqXHX7oy2PJevzRaR3YRa1De9m2MkQRyj480ESr_PPacSoh/pub?gid=0&single=true&output=csv"
-};
- 
